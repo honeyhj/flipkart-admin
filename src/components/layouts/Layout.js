f6649f98c8ea5a7849d8f67a7a2b244169227c46
Md Golam kibria <tenminuteversity@gmail.com>
2021-03-15 20:41:11 +0600

    hi

--- src/components/layouts/Layout.js
+++ src/components/layouts/Layout.js
@@ -28,19 +28,7 @@ const Layout = (props) => {
         :
         props.children
        }
-            <Header></Header>
-            {
-                props.sidebar ? (
-                    <>
-                    <ul>
-                        <li>categories</li>
-                        <li>products</li>
-                        <li>orders</li>
-                    </ul>
-                    </>
-                ):null
-            }
-            {props.children}
+           
         </>
     );
 };
